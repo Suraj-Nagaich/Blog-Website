c10f88bba0294572bb296c71aeb4a1d5576a0e30
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-08-01 00:10:07 +0530

    Update imagekit.js

--- src/utils/imagekit.js
+++ src/utils/imagekit.js
@@ -1,9 +1,9 @@
 const ImageKit = require("imagekit");
 require("dotenv").config();
 const imagekit = new ImageKit({
-  publicKey: process.env.public_iKERHi/EkubHXgB+fC39itJKVwM=,
-  privateKey: process.env.private_+qc6vSXnV5PdwRFWxJonzfKkOcQ=,
-  urlEndpoint:process.env.https://ik.imagekit.io/SURAJ/
+   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 module.exports = imagekit;
